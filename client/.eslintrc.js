1b3b434d8d5ac496d9358307a38891a41516cf86
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-08-10 15:39:20 +0100

    Add .eslintrc.js to configure ESLint for the client-side code, enforcing coding standards and best practices for React development, including rules for JSX and unused variables.

---
+++ client/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    'react/no-unescaped-entities': 'off',
+  },
+  globals: {
+    PublicKeyCredential: 'readonly',
+    confirm: 'readonly',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
